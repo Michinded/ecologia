267a4716073f7f05c6e0320f8ff47b5090f59147
Michinded <michinded@gmail.com>
2023-03-03 02:14:40 -0600

    Animación de carga
    Se agrego la animación de carga en la vista "blogtemplate" y el CSS en la carpeta public

--- resources/views/blogtemplate.blade.php
+++ resources/views/blogtemplate.blade.php
@@ -5,9 +5,13 @@
 	<meta charset="utf-8">
 	<link rel="icon" href="{{asset('images/iconweb.png')}}" type="image/x-icon">
 	<link rel="stylesheet" href="{{asset('css/style.css')}}">
+	<link rel="stylesheet" href="{{asset('css/carga.css')}}">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+<div class="loader-wrapper">
+      <div class="loader"></div>
+    </div>
 <header>
 <div class="pos-f-t">
   <nav class="navbar navbar-dark bg-dark fixed-top">
@@ -55,4 +59,10 @@
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script>
+      window.addEventListener("load", function () {
+        const loader = document.querySelector(".loader-wrapper");
+        loader.className += " hidden";
+      });
+    </script>
 </html>
