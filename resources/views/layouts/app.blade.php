d215093f88333beef17c0eb38e897fe67f7f72eb
Michinded <michinded@gmail.com>
2023-03-11 17:16:33 -0600

    Generar rutas a css
    Rutas definidas para css y el logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Icono de la pagina -->
-    <link rel="icon" scr="{{asset('images/panel2.png')}}" type="image/x-icon">
+    <link rel="icon" href="https://disociando.up.railway.app/images/panel2.png" type="image/x-icon">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -13,7 +13,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('css/formstyles.css')}}">
+    <link rel="stylesheet" href="https://disociando.up.railway.app/css/formstyles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <!-- Scripts -->
